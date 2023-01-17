22c39473c16ea910a28f1366ce2648136b6d2991
EquipeTrident <122904433+EquipeTrident@users.noreply.github.com>
2023-01-17 17:19:28 +0100

    Add files via upload

---
+++ SAE PREVIEW/Views/view_begin.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Gestion de Stages</title>
+		<link rel="stylesheet" href="./Content/css/GestionStage.css"/>
+	</head>
+	<body>
+		<nav>
+			<ul>
+				<li><a href="?controller=list&action=last"> 5 derniers étudiants</a></li>
+				<li><a href="?controller=list&action=all"> Tous les étudiants</a></li>
+			</ul>
+		</nav>
+
+		<header>
+			<h1><a href="?">Gestion Stages</a></h1>
+		</header>
+
+		<main>
---
+++ SAE PREVIEW/Views/view_bos.php
@@ -0,0 +1,5 @@
+<?php require "view_begin.php"; ?>
+
+<?php require "view_list_etudiant.php"; ?>
+
+<?php require "view_end.php"; ?>
