7a33ee3123cca004531b56dda27505311d3e144f
EquipeTrident <122904433+EquipeTrident@users.noreply.github.com>
2023-01-20 12:54:08 +0100

    Update Controller_login.php

--- SAE PREVIEW/Controllers/Controller_login.php
+++ SAE PREVIEW/Controllers/Controller_login.php
@@ -1,5 +1,9 @@
 <?php
 
+  /* La fonction ci dessous récupère des données d'une BD en la parcourant et rend une vue. C'est alors une fonction linéaire de compléxité O(n)
+    "La complexité de cet algorithme est dite linéaire. Ce sera le cas de tous les algorithmes avec T(n)an+b où a et b sont des réels." http://www.monlyceenumerique.fr/nsi_premiere/algo_a/a2_complexite.php
+    */
+
 class Controller_login extends Controller
 {
     public function action_login()
