0315640a36cc8dcd368b7eab21f69c1375ea1a68
EquipeTrident <122904433+EquipeTrident@users.noreply.github.com>
2023-01-20 12:55:32 +0100

    Update Controller_comment.php

--- SAE PREVIEW/Controllers/Controller_comment.php
+++ SAE PREVIEW/Controllers/Controller_comment.php
@@ -2,6 +2,9 @@
 
 class Controller_comment extends Controller
 {
+    /* Cette fonction récupère des données d'une BD en la parcourant et rend une vue. C'est alors une fonction linéaire de compléxité O(n)
+    "La complexité de cet algorithme est dite linéaire. Ce sera le cas de tous les algorithmes avec T(n)an+b où a et b sont des réels." http://www.monlyceenumerique.fr/nsi_premiere/algo_a/a2_complexite.php
+    */
     public function action_enrengistrer()
     {
 
