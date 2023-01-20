16ebe3533e12df9c0a148171842ec73e3a410625
EquipeTrident <122904433+EquipeTrident@users.noreply.github.com>
2023-01-20 01:40:01 +0100

    Add files via upload

--- saeee/Views/view_list_etudiant.php
+++ saeee/Views/view_list_etudiant.php
@@ -6,16 +6,16 @@
     <?php foreach ($liste as $pn) : ?>
     <tr>
         <td> <?= e($pn['student_id']) ?> </a></td>
-        <td> <?= e(ucfirst($pn['prenom'])) ?> </td>
-        <td> <?= e($pn['nom']) ?> </td>
-        <td> <?= e($pn['formation_id']) ?> </td>
+        <td> <?= e(ucfirst($pn['nom'])) ?> </td>
+        <td> <?= e($pn['prenom']) ?> </td>
+        <td> <?= e($pn['groupe']) ?> </td>
         <td class="sansBordure">
             <a href="?controller=list&action=informations&id=<?= e($pn['formation_id']) ?>">
                 <img class="icone" src="Content/img/profile-icon.png" alt="profile"/>
             </a>
         </td>
         <td class="sansBordure">
-            <a href="?controller=list&action=boslist&id=<?= e($pn['student_id'])?>">
+            <a href="?controller=list&action=boslist&id=<?= e($pn['Student_id'])?>">
                 <img class="icone" src="Content/img/edit-icon.png" alt="edit"/>
             </a>
         </td>
